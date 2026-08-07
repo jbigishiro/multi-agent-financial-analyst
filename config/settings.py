12a68e07e6f8fc3a54410f9a6edd36d141f2f173
Justin <justin.rvd@gmail.com>
2026-08-07 16:20:29 -0500

    Add centralized logging system

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Settings(BaseSettings):
 
     openai_api_key: str
@@ -15,6 +14,5 @@ class Settings(BaseSettings):
         env_file=".env",
         extra="ignore"
     )
-
 
 settings = Settings()
---
+++ test_logging.py
@@ -0,0 +1,18 @@
+from config.logging import logger
+
+
+logger.debug(
+    "This is debug"
+)
+
+logger.info(
+    "This is information"
+)
+
+logger.warning(
+    "This is warning"
+)
+
+logger.error(
+    "This is error"
+)
